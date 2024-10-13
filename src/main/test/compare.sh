371609ef950dde8fc56da6f9975d6d23787ef9a8
Virg So <virgilio.so@extremelogic.ph>
2024-10-13 16:53:06 +0800

    Added conditions in test script

--- src/main/test/compare.sh
+++ src/main/test/compare.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+VIDEO_FILE="./cc_minimum.ts"
+
+if [ ! -f "${VIDEO_FILE}" ]; then
+    echo "Video file '${VIDEO_FILE}' does not exist. Not proceeding to test. "
+    exit 1
+fi
+
 OUTPUT_C=c_output.txt
 OUTPUT_JAVA=java_output.txt
 DIFF_FILE=c_java_difference.txt
@@ -6,14 +15,16 @@ DIFF_FILE_TRUTH=diff_signature.txt
 DIFF_RESULTS=diff_results.txt
 
 TARGET_DIR=../../../target
-JAR_FILE=extremelogic-texttrack-0.0.1.jar
+JAR_FILE=extremelogic-texttrack-0.0.2-SNAPSHOT.jar
+
+printf "Running jar %s" "${JAR_FILE}. "
 
 java -jar ${TARGET_DIR}/${JAR_FILE} ./cc_minimum.ts > ${OUTPUT_JAVA} 
 
 diff ${OUTPUT_C} ${OUTPUT_JAVA} > ${DIFF_FILE}
 
 if diff ${DIFF_FILE} ${DIFF_FILE_TRUTH} > ${DIFF_RESULTS}; then
-  echo "Files are the same"
+  printf "Comparison [OK]\n"
 else
-  echo "Files are different"
+  printf "Comparison [NG]\n"
 fi
